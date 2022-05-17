6fbbe4f2ae09940b19bbc9a671ff643362cf8077
V-Srivatsan <vatsan.programming@gmail.com>
2022-05-17 11:02:41 +0530

    Changed GDAL and GEOS library paths

--- DashMed/settings.py
+++ DashMed/settings.py
@@ -105,12 +105,12 @@ DATABASES = {
 }
 
 import dj_database_url
-import os
 
 DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so'
+GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so'
 
 
 
